0f76be8030633abc56a9de5dd2a5b8bc558682af
Rashid Massanchi <rashid.massanchi@mail.utoronto.ca>
2023-01-07 01:01:06 -0500

    finally pushing all the files

---
+++ src/chatsPage.js
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <PrettyChatWindow
+        projectId="1849caee-4d3d-4e87-9182-0002644e7349"
+        username={props.user.username} // adam
+        secret={props.user.secret} // pass1234
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
